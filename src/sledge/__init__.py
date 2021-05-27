e45cbe71e00299bdfd3a0e1d931cc75cd4befe83
Filipe Verri <filipeverri@gmail.com>
2021-05-27 17:49:05 -0300

    Some stuff...

--- src/sledge/__init__.py
+++ src/sledge/__init__.py
@@ -5,7 +5,17 @@ Python package `sledge` semantic evaluation of clustering results.
 import pandas as pd
 import numpy as np
 
-def sledge_descriptors(X, labels):
+def sledge_descriptors(X, labels, minimum_support=0.0):
+    """
+    Parameters
+    ----------
+    X : array_like
+        TODO...
+    labels: list of ints
+        TODO...
+    minimum_support: float
+        Minimum support (percentage)
+    """
 
     nclusters = max(labels) + 1
 
@@ -14,7 +24,7 @@ def sledge_descriptors(X, labels):
 
     # 1-itemsets, for greater k
     support = X.groupby(pd.Series(labels)).apply(np.mean)
-    print(support)
+    print(support.transpose())
 
     # TODO: particularization and support threshold
 
--- tests/main.py
+++ tests/main.py
@@ -8,13 +8,13 @@ class TestSimpleCalculations(unittest.TestCase):
 
     def test_basic(self):
         X = pd.DataFrame.from_dict({
-            'A': [1, 0, 1, 0, 0, 0],
-            'B': [1, 0, 0, 0, 0, 0],
-            'C': [1, 1, 1, 1, 1, 1],
-            'D': [0, 0, 0, 1, 0, 0],
-            'E': [0, 0, 0, 1, 1, 0]})
+            'A': [1, 0, 1, 0, 0, 0, 0],
+            'B': [1, 0, 0, 0, 0, 0, 0],
+            'C': [1, 1, 1, 1, 1, 1, 0],
+            'D': [0, 0, 0, 1, 0, 1, 1],
+            'E': [0, 0, 0, 1, 1, 0, 1]})
 
-        labels = [0, 0, 0, 1, 1, 1]
+        labels = [0, 0, 0, 1, 1, 1, 1]
 
         score = sledge_score(X, labels)
         self.assertEqual(2, len(score))
