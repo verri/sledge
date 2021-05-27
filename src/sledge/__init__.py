3c995d89df9393de70c6cfa5c5ab00af169f3887
Filipe Verri <filipeverri@gmail.com>
2021-05-27 17:03:38 -0300

    Calculating support for each attribute.

--- src/sledge/__init__.py
+++ src/sledge/__init__.py
@@ -2,9 +2,28 @@
 Python package `sledge` semantic evaluation of clustering results.
 """
 
+import pandas as pd
+import numpy as np
+
 def sledge_descriptors(X, labels):
-    pass
+
+    nclusters = max(labels) + 1
+
+    if type(X) is np.ndarray:
+        X = pd.DataFrame(X, columns=[ f'X{i}' for i in range(X.shape[1]) ])
+
+    # 1-itemsets, for greater k
+    support = X.groupby(pd.Series(labels)).apply(np.mean)
+    print(support)
+
+    # TODO: particularization and support threshold
+
+    return support
 
 
 def sledge_score(X, labels, aggregation='harmonic', return_descriptors=False):
-    pass
+
+    nclusters = max(labels) + 1
+    descriptors = sledge_descriptors(X, labels)
+
+    return [ 0 for _ in range(nclusters) ]
---
+++ tests/main.py
@@ -0,0 +1,23 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+from sledge import sledge_score
+
+class TestSimpleCalculations(unittest.TestCase):
+
+    def test_basic(self):
+        X = pd.DataFrame.from_dict({
+            'A': [1, 0, 1, 0, 0, 0],
+            'B': [1, 0, 0, 0, 0, 0],
+            'C': [1, 1, 1, 1, 1, 1],
+            'D': [0, 0, 0, 1, 0, 0],
+            'E': [0, 0, 0, 1, 1, 0]})
+
+        labels = [0, 0, 0, 1, 1, 1]
+
+        score = sledge_score(X, labels)
+        self.assertEqual(2, len(score))
+
+if __name__ == '__main__':
+    unittest.main()
