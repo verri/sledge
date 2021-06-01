fe20ada94594c9875ece67a35b500689f172d24a
Filipe Verri <filipeverri@gmail.com>
2021-06-01 11:48:31 -0300

    Implemented particularization.

--- src/sledge/__init__.py
+++ src/sledge/__init__.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 
 
-def sledge_descriptors(X, labels, minimum_support=0.0):
+def semantic_descriptors(X, labels, minimum_support=0.0):
     """
     Parameters
     ----------
@@ -23,18 +23,38 @@ def sledge_descriptors(X, labels, minimum_support=0.0):
     if isinstance(X, np.ndarray):
         X = pd.DataFrame(X, columns=[f'X{i}' for i in range(X.shape[1])])
 
+    features = X.columns
+
     # 1-itemsets, for greater k
     support = X.groupby(pd.Series(labels)).apply(np.mean)
-    print(support.transpose())
 
-    # TODO: particularization and support threshold
+    # Particularization
+    for feature in features:
+        column = np.array(support[feature])
 
-    return support
+        max_support = np.array([np.max(np.delete(column, i))
+                               for i in range(nclusters)])
+        mean_support = np.array([np.mean(np.delete(column, i)) for i in
+                                 range(nclusters)])
+
+        toremove = column ** 2 <= mean_support * max_support
+        support.loc[toremove, feature] = 0
+
+    support[support < minimum_support] = 0
+    return support.transpose()
 
 
-def sledge_score(X, labels, aggregation='harmonic', return_descriptors=False):
+def sledge_score_clusters(
+        X,
+        labels,
+        aggregation='harmonic',
+        return_descriptors=False):
 
     nclusters = max(labels) + 1
-    descriptors = sledge_descriptors(X, labels)
+    descriptors = semantic_descriptors(X, labels)
 
     return [0 for _ in range(nclusters)]
+
+
+def sledge_score(X, labels):
+    return np.mean(sledge_score_clusters(X, labels))
--- tests/main.py
+++ tests/main.py
@@ -2,23 +2,23 @@ import unittest
 import pandas as pd
 import numpy as np
 
-from sledge import sledge_score
+from sledge import sledge_score_clusters
 
 
 class TestSimpleCalculations(unittest.TestCase):
 
     def test_basic(self):
         X = pd.DataFrame.from_dict({
-            'A': [1, 0, 1, 0, 0, 0, 0],
-            'B': [1, 0, 0, 0, 0, 0, 0],
-            'C': [1, 1, 1, 1, 1, 1, 0],
-            'D': [0, 0, 0, 1, 0, 1, 1],
-            'E': [0, 0, 0, 1, 1, 0, 1]})
+            'A': [1, 0, 1, 0, 0, 0, 0, 0],
+            'B': [1, 0, 0, 0, 0, 0, 0, 1],
+            'C': [1, 1, 1, 1, 1, 1, 0, 0],
+            'D': [0, 0, 0, 1, 0, 1, 1, 0],
+            'E': [0, 0, 0, 1, 1, 0, 1, 0]})
 
-        labels = [0, 0, 0, 1, 1, 1, 1]
+        labels = [0, 0, 0, 1, 1, 1, 1, 2]
 
-        score = sledge_score(X, labels)
-        self.assertEqual(2, len(score))
+        score = sledge_score_clusters(X, labels)
+        self.assertEqual(3, len(score))
 
 
 if __name__ == '__main__':
